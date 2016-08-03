f9024876b4f8aac1e0995e410edd6e5880d5cbb2
Hao Xiangpeng <3150102255@zju.edu.cn>
2016-08-03 10:51:36 +0800

    add even more test

--- api/tests.py
+++ api/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase, Client
-from django.http import HttpRequest
+import json
 from api.models import PersonInfo
 
 
@@ -65,7 +65,7 @@ class NewPostTest(TestCase):
             'user_agent': 'lkjas',
             'time_spend': '123456a'
         })
-        self.assertEqual(PersonInfo.objects.count(),0)
+        self.assertEqual(PersonInfo.objects.count(), 0)
 
     def test_new_post_can_overwrite_old_one(self):
         self.test_can_save_a_post()
@@ -91,3 +91,65 @@ class NewPostTest(TestCase):
         new_item = PersonInfo.objects.first()
         self.assertEqual(new_item.name, 'haoxiangpeng')
         self.assertEqual(new_item.share_work, 'nihao')
+
+
+class AuthenticTest(TestCase):
+    def test_success_auth(self):
+        c = Client()
+        response = c.post('/api/auth', {
+            'user_name': 'qscqscdadada',
+            'pass_word': 'ahaqsc'
+        })
+        json_content = json.loads(response.content.decode('utf-8'))[0]
+        self.assertEqual(json_content['login'], 'OK')
+        self.assertEqual(len(json_content['response_token']), 64)
+
+    def test_wrong_password(self):
+        c = Client()
+        response = c.post('/api/auth', {
+            'user_name': 'qscqscdadada',
+            'pass_word': 'qsc'
+        })
+        self.assertEqual(response.content, b'Authenticate failed')
+
+    def test_missing_param(self):
+        c = Client()
+        response = c.post('/api/auth', {
+            'user_name': 'qscqscdadada'
+        })
+        self.assertEqual(response.content, b'Authenticate failed')
+
+
+class RetrievePersonInfo(TestCase):
+    def test_can_retrieve_a_person(self):
+        c = Client()
+        response = c.post('/api/auth', {
+            'user_name': 'qscqscdadada',
+            'pass_word': 'ahaqsc'
+        })
+        json_content = json.loads(response.content.decode('utf-8'))[0]
+        token = json_content['response_token']
+        c.post('/api/save', {
+            'name': 'hao',
+            'student_id': '3140102255',
+            'gender': '2',
+            'major': 'CS',
+            'phone_number': '13208020663',
+            'self_intro': 'Hello World. Test driven development',
+            'question_one': 'Hello World. Test driven development',
+            'question_two': 'Hello World. Test driven development',
+            'inclination_one': '技术研发中心',
+            'inclination_two': '人力资源部门',
+            'share_work': 'lkjasdf',
+            'photo': 'photo',
+            'user_agent': 'lkjas',
+            'time_spend': '123456'
+        })
+        self.assertEqual(PersonInfo.objects.count(), 1)
+        detail_response = c.get('/api/detail', {
+            'cookie': token,
+            'student_id': '3140102255'
+        })
+        json_detail = json.loads(detail_response.content.decode('utf-8'))[0]
+        self.assertEqual(json_detail['fields']['name'], 'hao')
+        self.assertEqual(json_detail['fields']['photo'], 'photo')
